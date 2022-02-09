0d25f8b0da42c8edcb00be7b97fedb711e47fcf9
Skamer <Skamer@users.noreply.github.com>
2022-02-09 18:56:06 +0100

    remove a console log in error-handler middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,10 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../exceptions/http.exception";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-
   if (err instanceof HttpException) {
     return res.status(err.getStatus()).send(err.getResponse());
   }
-
-  console.log("Errror", err);
 };
