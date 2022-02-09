20507ccc954f9d829ed2a02908483728599749b0
Skamer <Skamer@users.noreply.github.com>
2022-02-09 18:51:26 +0100

    remove the references to old error classes in error-handler middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,15 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../exceptions/http.exception";
-import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({
-      statusCode: err.statusCode,
-      message: err.serializeErrors(),
-      error: err.name,
-    });
-  }
 
   if (err instanceof HttpException) {
     return res.status(err.getStatus()).send(err.getResponse());
