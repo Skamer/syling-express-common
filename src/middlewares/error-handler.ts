6ad8e90575152098cf3142b8ea0cb489fbfe0028
Skamer <Skamer@users.noreply.github.com>
2022-02-10 15:10:06 +0100

    The error handler middleware now catch all exceptions as fallback: show them in the server console, and return an Internal Server Error for client.

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../exceptions/http.exception";
+import { InternalServerErrorException } from "../exceptions/internal-server-error.exception";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof HttpException) {
     return res.status(err.getStatus()).send(err.getResponse());
   }
+
+  console.error(err)
+
+  const internalServerError = new InternalServerErrorException()
+  return res.status(internalServerError.getStatus()).send(internalServerError.getResponse())
 };
